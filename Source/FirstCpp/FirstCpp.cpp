88c0433a7bb29477e764c445e5a9cf7622155d7d
unknown <show959@hanmail.net>
2022-05-25 20:12:07 +0900

    first commit

---
+++ Source/FirstCpp/FirstCpp.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FirstCpp.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FirstCpp, "FirstCpp" );
